f58b4bb85e114f32bd03de8dffd28d514283c487
Debashree Jena <104843610+Debashree-10@users.noreply.github.com>
2025-04-16 15:57:42 +0000

    Implement 'use strict' mode in new JavaScript file

---
+++ 01_Foundations/02_StrictMode.js
@@ -0,0 +1,13 @@
+/*
+ * How to Use "use strict"; 
+ */
+
+// Globally: By placing "use strict"; at the top of a JavaScript file, it applies to the entire file.
+"use strict";
+// All code in this file is in strict mode
+
+// Locally: By placing "use strict"; inside a function, it applies only to that function.
+function myFunction() {
+    "use strict";
+    // Code here is in strict mode
+}
